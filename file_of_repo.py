a5fe3af17bd67df0738d3f7558202c7a21f7b0a7
rahulchandrawanshiji <161007517+rahulchandrawanshiji@users.noreply.github.com>
2025-03-08 14:12:11 +0530

    add the function
    function which use to the name identity and the subtitle and other changes

--- file_of_repo.py
+++ file_of_repo.py
@@ -2,4 +2,15 @@ print("hellow")
 print("122 2111111 3234444444444444 22 ")
 
 print("substitle and the management about the unnecssaer to divide and manipulation <br> ")
+user = []
+def sunstract_miner():
+    list={
+        "name" : "subtitle",
+        "prefrence":"allowed",
+        "devident": "two subs three "
+    }
+    user.append(list)
+    return sunstract_miner
 
+minimum = sunstract_miner()
+print(minimum)
